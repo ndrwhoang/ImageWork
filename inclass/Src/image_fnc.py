46537a80673199a602688d3aa047a3de1e287957
hoangqg <hoangqg@LCBLAB2110-09.uccob.uc.edu>
2020-02-25 09:18:21 -0500

    3rd commit

--- inclass/Src/image_fnc.py
+++ inclass/Src/image_fnc.py
@@ -9,3 +9,26 @@ import os, sys
 import requests
 from io import BytesIO
 
+def load_image( filename ) :
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+        return myimage
+    except:
+        print("Image load failed")
+        return None
+
+def save_image( imageObject, outfilename ) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    try:
+        imageObject.save( outfilename )
+    except:
+        print(".save() failed")
+        
+def crop_img(img, crop_size=(200,300,400,500)):   # an image object and a tuple  
+    im = img.crop(crop_size) # (left, top, right, bottom) it's a tuple!
+    return im
--- inclass/Src/main.py
+++ inclass/Src/main.py
@@ -5,6 +5,13 @@ Created on Feb 25, 2020
 '''
 from image_fnc import *
 
+'''
 # open an image file. The default path is where this python module is
 im = Image.open("SiriusAndViolet.jpg")
 print(im.width, im.height, im.mode, im.format)  # Display some info about the image
+'''
+my_image = load_image("SiriusAndViolet.jpg")
+#my_image.show(my_image)
+
+my_crop_image = crop_img(my_image)
+my_crop_image.show(my_crop_image)
